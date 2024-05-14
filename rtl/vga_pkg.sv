1c1bf764b4b1e402f040c81440efb0b43ce1c2d3
Andrzej Kozdrowski <andrzej.kozdrowski@wp.pl>
2024-05-14 11:31:28 +0200

    Corrected comments in vga_pkg

--- rtl/vga_pkg.sv
+++ rtl/vga_pkg.sv
@@ -16,18 +16,18 @@ localparam VER_PIXELS = 768;
 
 // Add VGA timing parameters here and refer to them in other modules.
 // horizontal 
-localparam H_COUNT_TOT = 1344; // 1056
-localparam H_SYNC_START = 1048; // 840
-localparam H_BLNK_START = 1024; // 800
-localparam H_SYNC_END = 1184; // 968
-localparam H_BLNK_END = 1344; //1056
+localparam H_COUNT_TOT = 1344; 
+localparam H_SYNC_START = 1048;
+localparam H_BLNK_START = 1024;
+localparam H_SYNC_END = 1184;
+localparam H_BLNK_END = 1344;
 
 // vertical 
-localparam V_COUNT_TOT = 806; // 628  
-localparam V_SYNC_START  = 771; //601
-localparam V_BLNK_START = 768; // 600
-localparam V_SYNC_END = 777; // 605
-localparam V_BLNK_END = 806; // 628
+localparam V_COUNT_TOT = 806;
+localparam V_SYNC_START  = 771;
+localparam V_BLNK_START = 768;
+localparam V_SYNC_END = 777;
+localparam V_BLNK_END = 806;
 
 //rect dimensions
 localparam RECT_WIDTH = 64;
